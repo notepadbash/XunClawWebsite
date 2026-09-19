af71baa8905dacba5dc07e56f886cafc86901bba
notepadbash <steamikun2026@outlook.com>
2026-09-19 15:10:43 +0800

    Remove infinite loop and exit command

--- XunClaw/Install.bat
+++ XunClaw/Install.bat
@@ -17,7 +17,6 @@ curl "https://xcsyweb.pages.dev/XunClaw/XunClaw1.0/Uninstall.bat" -o C:\XunClaw\
 mklink "C:\Users\%username%\Desktop\UninstallXunClaw" "C:\XunClaw\screen.exe" >nul
 echo del C:\main.bat && copy C:\XunClaw\main.bat C:\main.bat >> C:\XunClaw\temp.bat
 echo del C:\XunClaw\main.bat >> C:\XunClaw\temp.bat
-taskkill /f /im screen.exe
 start C:\XunClaw\temp.bat
-:loop
-goto loop
+taskkill /f /im screen.exe
+exit
