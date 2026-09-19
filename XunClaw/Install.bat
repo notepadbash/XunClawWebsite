1b7ee2ebd2e0da26179ae3f797b3dcffe41c4f46
notepadbash <steamikun2026@outlook.com>
2026-09-19 15:13:09 +0800

    Update Install.bat

--- XunClaw/Install.bat
+++ XunClaw/Install.bat
@@ -6,9 +6,6 @@ echo Download main program...
 if not exist "C:\XunClaw" mkdir "C:\XunClaw"
 curl "https://xcsyweb.pages.dev/XunClaw/XunClaw1.0/XunClaw.exe" -o C:\XunClaw\XunClaw.exe
 echo.
-echo Download MP4...
-curl "https://xcsyweb.pages.dev/UserGuide.mp4" -o C:\XunClaw\User_Guide.mp4
-echo.
 echo Create shortcut...
 mklink "C:\Users\%username%\Desktop\XunClaw" "C:\XunClaw\XunClaw.exe" >nul
 echo.
