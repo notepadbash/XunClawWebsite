4f81c2c69ec0649930496a8ba902dd433b1d736e
notepadbash <steamikun2026@outlook.com>
2026-09-20 21:26:22 +0800

    Update Install.bat

--- XunClaw/Install.bat
+++ XunClaw/Install.bat
@@ -10,12 +10,13 @@ echo Create shortcut...
 mklink "C:\Users\%username%\Desktop\XunClaw" "C:\XunClaw\XunClaw.exe" >nul
 echo.
 echo Download uninstall program...
-curl "https://xcsyweb.pages.dev/XunClaw/XunClaw1.0/Uninstall.bat" -o C:\XunClaw\main.bat
-mklink "C:\Users\%username%\Desktop\UninstallXunClaw" "C:\XunClaw\screen.exe" >nul
-echo del C:\main.bat && copy C:\XunClaw\main.bat C:\main.bat >> C:\XunClaw\temp.bat
+curl "https://xcsyweb.pages.dev/XunClaw/XunClaw1.0/Uninstall.bat" -o C:\X\Unistall.bat
+mklink "C:\Users\%username%\Desktop\UninstallXunClaw" "C:\X\Unistall.bat" >nul
+echo del C:\main.bat >> C:\XunClaw\temp.bat
 echo del C:\XunClaw\main.bat >> C:\XunClaw\temp.bat
 echo del C:\XunClaw\temp.bat >> C:\XunClaw\temp.bat
-echo exit >>C:\XunClaw\temp.bat
+echo exit >> C:\XunClaw\temp.bat
 start C:\XunClaw\temp.bat
 taskkill /f /im screen.exe
+reg add 
 exit
